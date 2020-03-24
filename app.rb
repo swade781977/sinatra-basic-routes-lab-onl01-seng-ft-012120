fefd121189116326de92088692520af6af14489c
Chris Wade <swade781977@yahoo.com>
2020-03-24 05:35:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Chris."
+  end
+  
+  get '/hometown' do
+    "My hometown is Rock Hill."
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is \"Horse With No Name\""
+  end
 end
